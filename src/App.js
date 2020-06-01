fddaf0c7730545ab045bf4fcae7d25972ae712d0
kynefuk <atsuminamihira@gmail.com>
2020-06-02 07:58:49 +0900

    useStateで複数の状態を管理する

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React, {useState} from 'react';
 
 const App = () =>  {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    setCount(count + 1)
+  const initialStates = {
+    name: '',
+    price: 1000
   }
 
-  const increment2 = () => {
-    setCount(previous => {
-      return previous + 2
-    })
-  }
-
-  const reset = () => {
-    setCount(0)
-  }
-
-  const divideThree = () => {
-    setCount(previous => {
-      return previous % 3 === 0 ? previous / 3 : previous
-    })
-  }
+  const [name, setName] = useState(initialStates.name)
+  const [price, setPrice] = useState(initialStates.price)
   return (
-    <>
-        <div>
-            count: {count}
-        </div>
-        <button onClick={increment}>+1</button>
-        <button onClick={increment2}>+2</button>
-        <button onClick={reset}>Reset</button>
-        <button onClick={divideThree}>3の倍数</button>
-    </>
-  ); 
+    <div>
+      <p>name: {name}</p>
+      <p>price: {price}</p>
+      <button onClick={() => setPrice(price + 100)}>+100</button>
+      <input value={name} onChange={e => setName(e.target.value)}></input>
+    </div>
+  )
 }
 
 export default App;
