0219d92d47db73cab1836cd0e94eb41f04376b09
kynefuk <atsuminamihira@gmail.com>
2020-06-02 07:16:27 +0900

    useStateをやった

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () =>  {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const increment2 = () => {
+    setCount(previous => {
+      return previous + 2
+    })
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const divideThree = () => {
+    setCount(previous => {
+      return previous % 3 === 0 ? previous / 3 : previous
+    })
+  }
   return (
-    <div>
-        This is a template for React app.
-    </div>
-  );
+    <>
+        <div>
+            count: {count}
+        </div>
+        <button onClick={increment}>+1</button>
+        <button onClick={increment2}>+2</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={divideThree}>3の倍数</button>
+    </>
+  ); 
 }
 
 export default App;
